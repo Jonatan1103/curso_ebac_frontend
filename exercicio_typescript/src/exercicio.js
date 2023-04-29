1f230838517262d1f67c89b2a26845b7b7a0d011
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2023-04-29 10:27:32 -0300

    finish

---
+++ exercicio_typescript/src/exercicio.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicar = (valorA, valorB) => valorA * valorB;
+const concatenar = (nome) => `Olá ${nome}`;
+console.log(multiplicar(10, 10));
+console.log(concatenar("Jonatan"));
---
+++ exercicio_typescript/src/exercicio.ts
@@ -0,0 +1,5 @@
+const multiplicar = (valorA: number, valorB: number): number => valorA * valorB
+const concatenar = (nome: string): string => `Olá ${nome}` 
+
+console.log(multiplicar(10, 10))
+console.log(concatenar("Jonatan"))
